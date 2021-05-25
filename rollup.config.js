ed954203710b9985ec688fede5d37126a67e18b4
Sebas <sebas.warsaw@gmail.com>
2021-05-25 09:14:48 -0300

    fix purge tw and exports cjs rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,53 @@
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import postcss from "rollup-plugin-postcss";
-import babel from "@rollup/plugin-babel";
-import replace from "@rollup/plugin-replace";
-import image from "@rollup/plugin-image";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import postcss from 'rollup-plugin-postcss';
+import babel from '@rollup/plugin-babel';
+import replace from '@rollup/plugin-replace';
+import image from '@rollup/plugin-image';
 // import livereload from "rollup-plugin-livereload";
 // import serve from "rollup-plugin-serve";
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
+
+const externals = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+  'react-scripts': 'ReactScripts',
+};
 
 export default {
-  input: "src/index.js",
+  input: 'src/index.js',
   output: [
     {
       file: packageJson.main,
-      format: "cjs",
+      format: 'cjs',
       sourcemap: true,
+      exports: 'named',
     },
     {
       file: packageJson.module,
-      format: "esm",
+      format: 'esm',
       sourcemap: true,
     },
   ],
-  external: [/@babel\/runtime/],
+  external: [/@babel\/runtime/, Object.keys(externals)],
   plugins: [
     image(),
     postcss({
-      extensions: [".css"],
+      extensions: ['.css'],
     }),
     nodeResolve({
-      extensions: [".js", ".jsx"],
+      extensions: ['.js', '.jsx'],
     }),
     peerDepsExternal(),
     replace({
-      "process.env.NODE_ENV": JSON.stringify("development"),
+      'process.env.NODE_ENV': JSON.stringify('production'),
       preventAssignment: true,
     }),
     babel({
-      babelHelpers: "runtime",
-      exclude: "!(node_modules/@storybook/addons/src/index.js)node_modules/**",
+      babelHelpers: 'runtime',
+      exclude: '!(node_modules/@storybook/addons/src/index.js)node_modules/**',
     }),
     commonjs(),
     // serve({
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/components/**/*.tsx',
+  ],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {},
