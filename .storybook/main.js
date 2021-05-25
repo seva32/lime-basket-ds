2583298fdf2ae4078e1f030d1b64953d09e61d76
Sebas <sebas.warsaw@gmail.com>
2021-05-24 22:19:26 -0300

    postcss ok

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,17 @@
 module.exports = {
   stories: ["../src/**/*.stories.@(js|jsx)"],
-  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
   babel: async (options) => ({
     ...options,
     // any extra options you want to set
