3e0c789e7bed698c44741ba7097eb6721b507245
Sebas <sebas.warsaw@gmail.com>
2021-07-10 12:50:06 -0300

    section component fix export closes #9

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ import './styles/tailwind.css';
 
 export { Button } from './components/Button';
 export { Card } from './components/Card';
+export { Link } from './components/Link';
+export { Section } from './components/Section';
